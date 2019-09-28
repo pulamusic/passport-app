7914e6bd05f564760b356b8e6e79736fe02d709b
Jim Carroll <jim@pulamusic.com>
2019-09-28 11:16:06 -0400

    added views

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # NodeJS With Passport Authentication
 
 This repository holds the code for a [Traversy Media](https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA) YouTube video on [Passport authentication](https://youtu.be/6FOq4cUdH8k).
+
+---
+
+**FUCK**: As of 2019-09-28 this app doesn't work because there is something screwed up with the ejs module. I need to figure this out before moving on.
---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+
+const app = express()
+
+// EJS Middleware
+app.use(expressLayouts)
+app.set('view engine', 'ejs')
+
+// Routes
+app.use('/', require('./routes/index.js'))
+app.use('/users', require('./routes/users.js'))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running on port ${PORT}.`))
