cb019996e3ee758be259142a50240d58cbd87011
Jim Carroll <jim@pulamusic.com>
2019-10-02 13:58:34 -0400

    worked a bit more but not connecting with database

--- README.md
+++ README.md
@@ -7,5 +7,8 @@ This repository holds the code for a [Traversy Media](https://www.youtube.com/ch
 **FUCK**: As of 2019-09-28 this app doesn't work because there is something screwed up with the ejs module. I need to figure this out before moving on.
 * Figured it out. I needed to have run `npm install ejs` in addition to installing `express-ejs-layouts`.
 * (2019-09-30) The page is not rendering as if it is HTML, though. I'll need to sort this out.
+* (2019-10-02) Got it. Moving on.
+
+---
 
 [`passport-auth` database](https://cloud.mongodb.com/v2/5d8f614dc56c9839eee37609#clusters)
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
+
+// User model
+const User = require('../models/User')
 
 // Login Page
 router.get('/login', (req, res) => res.render('login'))
@@ -36,7 +40,30 @@ router.post('/register', (req, res) => {
       password2
     })
   } else {
-    res.send('pass')
+    // validation passed
+    User.findOne({ email: email })
+      .then(user => {
+        if (user) {
+          // User exists
+          errors.push({ msg: 'Email is already registered' })
+          res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+          })
+        } else {
+          const newUser = new User({
+            name,
+            email,
+            password
+          })
+
+          console.log(newUser)
+          res.send('Hello')
+        }
+      })
   }
 })
 
