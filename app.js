4a35f522f875fdf3abed56224c76ca72d63bb225
Jim Carroll <jim@pulamusic.com>
2019-10-02 13:37:18 -0400

    added login and register forms with validation

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
+const mongoose = require('mongoose')
 
 const app = express()
+
+// DB config
+const db = require('./config/key').MongoURI
+
+// connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
 
 // EJS Middleware
 app.use(expressLayouts)
 app.set('view engine', 'ejs')
+
+// Bodyparser
+app.use(express.urlencoded({ extended: false }))
 
 // Routes
 app.use('/', require('./routes/index.js'))
---
+++ config/key.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MongoURI: 'mongodb+srv://pulamusic:Zanaya.2006@passport-auth-hvlt7.mongodb.net/test?retryWrites=true&w=majority'
+}
